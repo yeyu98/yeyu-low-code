932a422fbbc2f4bb7f165438ac4f002a10de1499
xiaohu <zhouyi@yx.com>
2023-06-20 17:25:15 +0800

    feat: app add

---
+++ apps/editor/index.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: xiaohu
+ * @Date: 2023-06-20 17:24:51
+ * @LastEditors: xiaohu
+ * @LastEditTime: 2023-06-20 17:24:52
+ * @FilePath: \yeyu-low-code\apps\editor\index.ts
+ * @Description: 
+ */
